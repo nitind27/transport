0848bf9d7247faf66479e575bb7fcb5e581541c8
Nitin dube <nitindube227@gmail.com>
2025-09-19 15:33:37 +0530

    add

--- src/app/(admin)/(others-pages)/dipatchdetials/page.tsx
+++ src/app/(admin)/(others-pages)/dipatchdetials/page.tsx
@@ -14,7 +14,7 @@ const page = async () => {
     return (
         <div className="grid grid-cols-6 gap-4 md:gap-6">
             <div className="col-span-12 space-y-6 xl:col-span-7">
-                <Breadcrumbs title="Dipatch Detials" breadcrumbs={breadcrumbItems} />
+                <Breadcrumbs title="Dipatch Details" breadcrumbs={breadcrumbItems} />
                 <Dipatchdetials />
             </div>
         </div>
